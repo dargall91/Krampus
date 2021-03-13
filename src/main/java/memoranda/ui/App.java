cd0a3f876bc0faeab73affd21ecf976cdc4860bf
Chris Boveda <cboveda@asu.edu>
2021-03-12 21:46:34 -0800

    Change version and build info

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -35,8 +35,8 @@ public class App {
        The actual values are substituted by the Ant build script using 
        'version' property and datestamp.*/
 
-	public static final String VERSION_INFO = "@VERSION@";
-	public static final String BUILD_INFO = "@BUILD@";
+	public static final String VERSION_INFO = "V1.0";
+	public static final String BUILD_INFO = "Gradle 6.6.1";
 	
 	/*========================================================================*/
 
