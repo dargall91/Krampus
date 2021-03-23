cc887529a9dd7e5c0ca72e7d9ca03d1c27a384ac
Chris Boveda <cboveda@asu.edu>
2021-03-22 20:58:53 -0700

    Correct all instances of incorrect language

--- src/main/java/memoranda/util/AgendaGenerator.java
+++ src/main/java/memoranda/util/AgendaGenerator.java
@@ -367,8 +367,8 @@ public class AgendaGenerator {
 				.class
 				.getResource("/ui/agenda/removesticker.gif")
 				.toExternalForm();
-		 String s = "<hr><hr><table border=\"0\" cellpadding=\"0\" width=\"100%\"><tr><td><a href=\"memoranda:importstickers\"><b>"+Local.getString("Importar anotaci�n")+"</b></a></td><td><a href=\"memoranda:exportstickerst\"><b>"+Local.getString("Exportar anotaci�n como .txt")+"</b></a><td><a href=\"memoranda:exportstickersh\"><b>"+Local.getString("Exportar anotaci�n como .html")+"</b></a></td></tr></table>"
-				 +   "<table border=\"0\" cellpadding=\"0\" width=\"100%\"><tr><td><a href=\"memoranda:addsticker\"><img align=\"left\" width=\"22\" height=\"22\" src=\""				
+		 String s = "<hr><hr><table border=\"0\" cellpadding=\"0\" width=\"100%\"><tr><td><a href=\"memoranda:importstickers\"><b>"+Local.getString("Import stickers")+"</b></a></td><td><a href=\"memoranda:exportstickerst\"><b>"+Local.getString("Export stickers in txt")+"</b></a><td><a href=\"memoranda:exportstickersh\"><b>"+Local.getString("Export stickers in html")+"</b></a></td></tr></table>"
+				 +   "<table border=\"0\" cellpadding=\"0\" width=\"100%\"><tr><td><a href=\"memoranda:addsticker\"><img align=\"left\" width=\"22\" height=\"22\" src=\""
 				 + iurl
 				+ "\" border=\"0\"  hspace=\"0\" vspace=\"0\" alt=\"New sticker\"></a></td><td width=\"100%\"><a href=\"memoranda:addsticker\"><b>&nbsp;"
 				+Local.getString("Add sticker")+"</b></a></td></tr></table>";
@@ -421,7 +421,7 @@ public class AgendaGenerator {
 		s += generateAllProjectsInfo(date, expandedTasks);
 		s += generateEventsInfo(date);
 		s += generateStickers(date);
-		//        /*DEBUG*/System.out.println(s+FOOTER);
+		//      /*DEBUG*/System.out.println(s+FOOTER);
 		return s + FOOTER;
 	}
 	/*    
