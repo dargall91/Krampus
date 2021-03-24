d18755bd81ba65060787c015293e08331203b9ab
bpape1usa <79953801+bpape1usa@users.noreply.github.com>
2021-03-24 10:10:30 -0700

    Merge US1 - JUnit5, Nodes and JSON I/O (#20)
    * Add Node and Coordinate classes (WIP)
    
    * US1-wip stub load/save file routines
    
    * Continue work on NodeColl and Coordinate
    
    * Create utility functions
    
    * Fix window close operation
    
    Program now poroperly terminates when "X" is clicked
    
    * Fix window minimize
    
    Application now properly minimizes
    
    * Remove unused code
    
    Removed a bit of code that was used for trying to fix the minimize operation
    
    * update for Eclipse
    
    * Fix so the preferences open
    
    * Remove Look and Feel from preferences
    
    * Reorganize the reference materials
    
    * Add class diagrams for core, ui, util
    
    * Add meeting minutes and updates
    
    * add logo to repo
    
    * Add JSONlist prototype
    
    * Add Jackson JSON jar files
    
    * Update gradle build file for Jackson
    
    * Chnage attribute names for JSON export
    
    * WIP: JSON Export code
    
    * Prototype single node export to JSON.
    
    * Add temporary json icons
    
    * Back out GUI changes in favor of JUnit
    
    * Add JUnit5 to gradle configuration
    
    * Create initial JUnit5 test class
    
    * Test array output for JSON node file
    
    * Pull in JSON work from US1-wip
    
    * Test file writer and exception handling
    
    * Correct all instances of incorrect language
    
    * Prototype json load from file
    
    * Finish test of json load routine
    
    * Add javadocs to Node infrastructure methods
    
    * Add comment to build.gradle
    
    * Update import statements
    
    * Update imports
    
    * Re-visit import optimization and wildcards
    
    Co-authored-by: kmdolan2 <74839153+kmdolan2@users.noreply.github.com>
    Co-authored-by: Derek Argall <dargall91@comcast.net>
    Co-authored-by: John Thurstonson <jrthurst@asu.edu>
    Co-authored-by: Chris Boveda <cboveda@asu.edu>
    Co-authored-by: kevinmdolan <kmdolan2@asu.edu>

---
+++ src/main/java/memoranda/Node.java
@@ -0,0 +1,98 @@
+package main.java.memoranda;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ *
+ */
+public class Node {
+    private int id;
+    private String name;
+    private Coordinate coords;
+
+    /**
+     *
+     * @param id
+     * @param name
+     * @param lat
+     * @param lon
+     */
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public Node(@JsonProperty("id") int id, @JsonProperty("name") String name, @JsonProperty("lat") Double lat,
+                @JsonProperty("lon") Double lon){
+        this.id=id;
+        this.name=name;
+        coords=new Coordinate(lat, lon);
+    }
+
+    /**
+     * return distance to another node
+     * @param n
+     * @return
+     */
+    public Double distanceTo(Node n){
+        return this.coords.distanceTo(n.getCoords());
+    }
+    /**
+     * Standard getter for latitude
+     * @return
+     */
+    public Double getLat(){
+        return coords.getLat();
+    }
+
+    /**
+     * Standard getter for longitude
+     * @return
+     */
+    public Double getLon(){
+        return coords.getLon();
+    }
+
+    @JsonIgnore
+    public Coordinate getCoords(){
+        return coords;
+    }
+    /**
+     * standard getter for name
+     * @return
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * standard getter for id
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * standard toString()
+     * @return
+     */
+    @Override
+    public String toString(){
+        return id+":"+"'"+name+"'@"+ coords;
+    }
+
+    // file name will be .nodes in home directory
+    /*
+    The input file for the system will be in json format with specific key/value pairs that define the following, defined as “nodes”:
+
+- unique numeric ID
+- textual description
+- latitude
+- longitude
+
+A data class and collection class will be defined to hold, aggregate, and access the data.
+
+AC1: JSON file contains all required data fields
+
+AC2: Data are exposed in a manner accessible to the application through an object with a defined interface.
+     */
+}
