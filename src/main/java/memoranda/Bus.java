d67cd977e6261b8d655a12721a5004b4d6c13d16
Brian Pape <bpape1@asu.edu>
2021-04-02 16:13:24 -0700

    Complete Bus and BusColl

--- src/main/java/memoranda/Bus.java
+++ src/main/java/memoranda/Bus.java
@@ -1,4 +1,59 @@
 package main.java.memoranda;
 
-public class Bus {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ *
+ */
+public class Bus extends IndexedObject{
+    private int number;
+
+    /**
+     *
+     * @param id
+     */
+    Bus(Integer id) {
+        super(id);
+    }
+
+
+    /**
+     *
+     * @param id
+     * @param number
+     */
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    Bus(@JsonProperty("id") Integer id, @JsonProperty("number") int number){
+        this(id);
+        this.number=number;
+    }
+
+
+
+    /**
+     *
+     * @param number
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getNumber() {
+        return number;
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return "bus id "+getID()+" number "+getNumber();
+    }
 }
--- src/main/java/memoranda/BusColl.java
+++ src/main/java/memoranda/BusColl.java
@@ -1,4 +1,94 @@
 package main.java.memoranda;
 
-public class BusColl {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import main.java.memoranda.util.DuplicateKeyException;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class BusColl extends DataCollection<Bus> implements Iterable<Bus>{
+
+    /**
+     * create a new node collection
+     */
+    public BusColl(){
+        super();
+    }
+
+
+
+    /**
+     * add an entire collection of nodes (post json import)
+     *
+     * @param c
+     */
+    public BusColl(Collection<Bus> c) throws DuplicateKeyException {
+        this();
+        for (Bus b:c){
+            add(b);
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Bus newItem(){
+        return new Bus(getUniqueID());
+    }
+
+
+    /**
+     * get bus by ID
+     * @param id
+     * @return
+     */
+    public Bus get(int id){
+        return (Bus)super.get(id);
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    @JsonProperty
+    public Collection<IndexedObject> getBuses(){
+        return getData();
+    }
+
+
+
+    /**
+     * iterator
+     * @return
+     */
+    @Override
+    public Iterator<Bus> iterator() {
+        return new BusColl.BusIterator();
+    }
+
+    /**
+     * iterator
+     * @param <Bus>
+     */
+    public class BusIterator<Bus> implements Iterator<Bus>{
+        Collection coll;
+        Iterator<Bus> it;
+        public BusIterator(){
+            coll=getData();
+            it=coll.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return it.hasNext();
+        }
+
+        @Override
+        public Bus next() {
+            return it.next();
+        }
+    }
 }
