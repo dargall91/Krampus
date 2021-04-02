aca3ac87e68919b34214e777c12619134db9a026
Brian Pape <bpape1@asu.edu>
2021-04-02 10:23:57 -0700

    US82, US37: Add placeholder for database and Bus classes

---
+++ src/main/java/memoranda/Database.java
@@ -0,0 +1,9 @@
+package main.java.memoranda;
+
+public class Database {
+    private NodeColl nodes;
+    private RouteColl routes;
+    private TourColl tours;
+    private DriverColl drivers;
+    private BusColl buses;
+}
