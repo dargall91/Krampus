b62454065bbad6e15a61e16aab1381450247ebdf
Derek Argall <dargall91@comcast.net>
2021-04-07 17:27:38 -0400

    Create Bus UI components (US7: Task 9 & 10)
    Driver UI Components were copied and ranmed to be ralted to Buses; began converting BusPanel from displaying Driver data to Bus data

---
+++ src/main/java/memoranda/ui/BusScheduleTable.java
@@ -0,0 +1,300 @@
+package main.java.memoranda.ui;
+
+import main.java.memoranda.CurrentProject;
+import main.java.memoranda.Driver;
+import main.java.memoranda.DriverColl;
+import main.java.memoranda.Tour;
+import main.java.memoranda.TourColl;
+import main.java.memoranda.util.CurrentStorage;
+import main.java.memoranda.util.Local;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+/**
+ * DriverScheduleTable is a JTable that contains the data related to a Driver's Schedule (Tour Name, bus ID, tour ID, date, time)
+ * 
+ * @author Derek Argall
+ * @version 04/05/2020
+ */
+public class BusScheduleTable extends JTable {
+    private ArrayList<Tour> tours;
+    private Driver driver;
+    private TourColl tourColl;
+    private DriverColl driverColl;
+    private TableRowSorter<TableModel> sorter;
+    private final int HEIGHT = 24;
+
+    /**
+     * Constructor for DriverScheduleTable. Sets a default driver
+     * 
+     * @param driver The default driver's who's schedule will be displayed
+     */
+    public BusScheduleTable(Driver driver) {
+        super();
+        setDriver(driver);
+        init();
+    }
+
+    /**
+     * Constructor for DriverScheduleTable to be used when DriverColl is empty
+     */
+    public BusScheduleTable() {
+		super();
+		setDriver(null);
+        init();
+	}
+
+	private void init() {
+		setToolTipText("Click to select a tour. Right-click for options.");
+		
+		driverColl = CurrentProject.getDriverColl();
+		tourColl = CurrentProject.getTourColl();
+    	
+    	JPopupMenu optionsMenu = new JPopupMenu();
+    	optionsMenu.setFont(new Font("Dialog", 1, 10));
+    	
+    	JMenuItem removeTour = new JMenuItem("Remove Tour");
+    	removeTour.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				removeActionEvent(e);
+			}
+    	});
+    	
+    	optionsMenu.add(removeTour);
+    	
+    	addMouseListener(new MouseAdapter() {
+    		public void mousePressed(MouseEvent e) {
+    			maybeShowPopup(e);
+    		}
+
+    		public void mouseReleased(MouseEvent e) {
+    			maybeShowPopup(e);
+    		}
+
+    		private void maybeShowPopup(MouseEvent e) {
+    			if (e.isPopupTrigger()) {
+    				optionsMenu.show(e.getComponent(), e.getX(), e.getY());
+    			}
+    		}
+    	});
+    	
+    	setRowHeight(HEIGHT);
+        setShowGrid(false);
+        
+        ScheduleTableModel model = new ScheduleTableModel();
+        setModel(model);
+        
+        sorter = new TableRowSorter<>(model);
+        setRowSorter(sorter);
+        
+        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        if (tours.size() > 0) {
+        	setRowSelectionInterval(0, 0);
+        }
+        
+        setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
+        
+        initColumnsWidth();
+    }
+
+    private void initColumnsWidth() {
+    	for (int i = 0; i < getColumnCount(); i++) {
+		    TableColumn tableColumn = getColumnModel().getColumn(i);
+		    int preferredWidth = tableColumn.getMinWidth();
+		    int maxWidth = tableColumn.getMaxWidth();
+		
+		    for (int j = 0; j < getRowCount(); j++) {
+		        TableCellRenderer cellRenderer = getCellRenderer(j, i);
+		        Component comp = prepareRenderer(cellRenderer, j, i);
+		        int width = comp.getPreferredSize().width + getIntercellSpacing().width;
+		        preferredWidth = Math.max(preferredWidth, width);
+		
+		        if (preferredWidth >= maxWidth) {
+		            preferredWidth = maxWidth;
+		            break;
+		        }
+		    }
+		
+		    tableColumn.setMinWidth(preferredWidth);
+		}
+    }
+
+    /**
+     * Repaints the table to reflect any changes to the data
+     */
+    public void tableChanged() {
+        init();
+        updateUI();
+    }
+
+    /**
+     * Defines how to render a cell
+     */
+    public TableCellRenderer getCellRenderer(int row, int column) {
+        return new javax.swing.table.DefaultTableCellRenderer() {
+            public Component getTableCellRendererComponent(
+                    JTable table,
+                    Object value,
+                    boolean isSelected,
+                    boolean hasFocus,
+                    int row,
+                    int column) {
+            	JLabel comp = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                
+                if (((row % 2) > 0) && (!isSelected)) {
+                    comp.setBackground(new Color(230, 240, 255));
+                }
+                
+                return comp;
+            }
+        };
+    }
+
+    private class ScheduleTableModel extends AbstractTableModel {
+        private String[] columnNames = {
+                Local.getString("Name"),
+                Local.getString("Tour ID"),
+                Local.getString("Bus ID"),
+                //Local.getString("Date"),
+                Local.getString("Time")};
+
+        public String getColumnName(int i) {
+            return columnNames[i];
+        }
+
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        public int getRowCount() {
+        	if (tours == null) {
+        		return 0;
+        	}
+        	
+            return tours.size();
+        }
+
+        public Object getValueAt(int row, int col) {
+            //String tour = "Tour";
+        	Tour tour = driver.getTours().toArray(new Tour[tours.size()])[row];
+
+            if (col == 0) {
+            	return tour.getName();
+            }
+            
+            if (col == 1) {
+            	return tour.getID();
+            }
+            
+            if (col == 2) {
+            	return tour.getBusID();
+            }
+            
+            if (col == 3) {
+            	return tour.getTime();
+            }
+            
+            /*if (col == 4) {
+            	return tour.getDate();
+            }*/
+
+            return null;
+        }
+
+        public Class getColumnClass(int col) {
+        	for (int i = 0; i < getRowCount(); i++) {
+        		Object obj = getValueAt(i, col); {
+        			if (obj != null) {
+        				return obj.getClass();
+        			}
+        		}
+        	}
+
+        	//default to String
+            return String.class;
+        }
+    }
+    
+    private void removeActionEvent(ActionEvent e) {
+    	Tour tour = getTour();
+    	int result = JOptionPane.showConfirmDialog(null,  "Remove Tour from " + driver.getName() + "'s Schedule?", "Delete Tour", JOptionPane.OK_CANCEL_OPTION);
+    	
+    	if (result == JOptionPane.OK_OPTION) {
+    		driver.delTour(tour);
+    		
+    		try {
+				CurrentStorage.get().storeDriverList(CurrentProject.get(), driverColl);
+				CurrentStorage.get().storeTourList(CurrentProject.get(), tourColl);
+				setDriver(driver);
+				tableChanged();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+    	}
+    }
+    
+    /**
+     * Sets the driver who's schedule is to be displayed
+     * 
+     * @param driver The driver to display the schedule for
+     */
+    public void setDriver(Driver driver) {
+    	this.driver = driver;
+    	
+    	if (driver == null) {
+    		tours = new ArrayList<Tour>();
+    	}
+    	
+    	else {
+    		tours = new ArrayList<Tour>(driver.getTours());    		
+    	}
+    }
+    
+    /**
+     * Gets the driver who's schedule is being displayed
+     * 
+     * @param driver The driver to display the schedule for
+     */
+    public Driver getDriver() {
+    	return driver;
+    }
+    
+    /**
+     * Gets the currently selected tour
+     * 
+     * @return the Tour
+     */
+    private Tour getTour() {
+    	return tours.get(getSelectedRow());
+    }
+    
+    /**
+     * Add a tour to this table
+     * 
+     * @param tour The tour  to be added
+     */
+    public void addTour(Tour tour) {
+    	tours.add(tour);
+    }
+}
---
+++ src/main/java/memoranda/ui/BusTable.java
@@ -0,0 +1,313 @@
+package main.java.memoranda.ui;
+
+import main.java.memoranda.CurrentProject;
+import main.java.memoranda.Driver;
+import main.java.memoranda.DriverColl;
+import main.java.memoranda.Tour;
+import main.java.memoranda.TourColl;
+import main.java.memoranda.util.CurrentStorage;
+import main.java.memoranda.util.Local;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.LinkedList;
+
+/**
+ * DriverTable is a JTable that contains the data related to a Driver (name, ID, and phone number)
+ * 
+ * @author Derek Argall
+ * @version 04/05/2020
+ */
+public class BusTable extends JTable {
+    private DriverColl drivers;
+    private TableRowSorter<TableModel> sorter;
+    private final int HEIGHT = 24;
+    private final int ID_COLUMN = 1;
+    private BusScheduleTable scheduleTable;
+
+    /**
+     * Constructor for a DriverTable
+     */
+    public BusTable() {
+        super();
+        init();   
+    }
+
+    private void init() {
+    	setToolTipText("Click a driver to display their schedule. Right-click for options.");
+    	
+    	drivers = CurrentProject.getDriverColl();
+    	
+    	JPopupMenu optionsMenu = new JPopupMenu();
+    	optionsMenu.setFont(new Font("Dialog", 1, 10));
+    	
+    	JMenuItem editDriver = new JMenuItem("Edit Driver");
+    	editDriver.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				editActionEvent(e);
+			}
+    	});
+    	
+    	JMenuItem deleteDriver = new JMenuItem("Delete Driver");
+    	deleteDriver.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				deleteActionEvent(e);
+			}
+    	});
+    	
+    	optionsMenu.add(editDriver);
+    	optionsMenu.add(deleteDriver);
+    	
+    	addMouseListener(new MouseAdapter() {
+    		public void mouseClicked(MouseEvent e) {
+    			scheduleTable.setDriver(getDriver());
+    			scheduleTable.tableChanged();
+    		}
+    		
+    		public void mousePressed(MouseEvent e) {
+    			maybeShowPopup(e);
+    		}
+
+    		public void mouseReleased(MouseEvent e) {
+    			maybeShowPopup(e);
+    		}
+
+    		private void maybeShowPopup(MouseEvent e) {
+    			if (e.isPopupTrigger()) {
+    				optionsMenu.show(e.getComponent(), e.getX(), e.getY());
+    			}
+    		}
+    	});
+        
+        setRowHeight(HEIGHT);
+        setShowGrid(false);
+        
+        DriverTableModel model = new DriverTableModel();
+        setModel(model);
+        
+        sorter = new TableRowSorter<>(model);
+        setRowSorter(sorter);
+        
+        setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        if (drivers.size() >= 1) {
+        	setRowSelectionInterval(0, 0);
+        }
+        
+        setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
+        
+        initColumnsWidth();
+    }
+
+    private void initColumnsWidth() {
+    	//dynamically set column width
+		for (int i = 0; i < getColumnCount(); i++) {
+		    TableColumn tableColumn = getColumnModel().getColumn(i);
+		    int preferredWidth = tableColumn.getMinWidth();
+		    int maxWidth = tableColumn.getMaxWidth();
+		
+		    for (int j = 0; j < getRowCount(); j++) {
+		        TableCellRenderer cellRenderer = getCellRenderer(j, i);
+		        Component comp = prepareRenderer(cellRenderer, j, i);
+		        int width = comp.getPreferredSize().width + getIntercellSpacing().width;
+		        preferredWidth = Math.max(preferredWidth, width);
+		
+		        if (preferredWidth >= maxWidth) {
+		            preferredWidth = maxWidth;
+		            break;
+		        }
+		    }
+		
+		    tableColumn.setMinWidth(preferredWidth);
+		}
+    }
+
+    /**
+     * Repaints the table to reflect any changes to the data
+     */
+    public void tableChanged() {
+        init();
+        updateUI();
+    }
+
+    /**
+     * Defines how to render a cell
+     */
+    public TableCellRenderer getCellRenderer(int row, int column) {
+        return new javax.swing.table.DefaultTableCellRenderer() {
+            public Component getTableCellRendererComponent(
+                    JTable table,
+                    Object value,
+                    boolean isSelected,
+                    boolean hasFocus,
+                    int row,
+                    int column) {
+                JLabel comp = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                
+                if (((row % 2) > 0) && (!isSelected)) {
+                    comp.setBackground(new Color(230, 240, 255));
+                }
+                
+                return comp;
+            }
+        };
+    }
+
+    /**
+     * Defines the table model for the Driver Table
+     */
+    private class DriverTableModel extends AbstractTableModel {
+        private String[] columnNames = {
+                Local.getString("Name"),
+                Local.getString("ID"),
+                Local.getString("Phone Number")};
+
+        public String getColumnName(int i) {
+            return columnNames[i];
+        }
+
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        public int getRowCount() {
+            return drivers.size();
+        }
+
+        public Object getValueAt(int row, int col) {
+            //set the selected driver for use by other methods in addition to displaying information
+            Driver driver = drivers.getDrivers().toArray(new Driver[drivers.size()])[row];
+
+            if (col == 0) {
+            	return driver.getName();
+            }
+            
+            if (col == 1) {
+            	return driver.getID();
+            }
+            
+            if (col == 2) {
+            	return driver.getPhoneNumber();
+            }
+
+            return null;
+        }
+
+        public Class getColumnClass(int col) {
+        	for (int i = 0; i < getRowCount(); i++) {
+        		Object obj = getValueAt(i, col); {
+        			if (obj != null) {
+        				return obj.getClass();
+        			}
+        		}
+        	}
+
+        	//default to String
+            return String.class;
+        }
+    }
+    
+    private void editActionEvent(ActionEvent e) {
+    	DriverDialog dlg = new DriverDialog(App.getFrame(), "Edit Driver", "Edit");
+    	Driver driver = getDriver();
+    	dlg.setName(driver.getName());
+    	dlg.setPhone(driver.getPhoneNumber());
+    	
+    	Dimension frmSize = App.getFrame().getSize();
+		Point loc = App.getFrame().getLocation();
+		
+		dlg.setLocation(
+				(frmSize.width - dlg.getSize().width) / 2 + loc.x,
+				(frmSize.height - dlg.getSize().height) / 2
+						+ loc.y);
+		dlg.setVisible(true);
+		
+		if (!dlg.isCancelled()) {
+			driver.setName(dlg.getName());
+			driver.setPhoneNumber(dlg.getPhone());
+			
+			try {
+				CurrentStorage.get().storeDriverList(CurrentProject.get(), drivers);
+				tableChanged();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+    }
+    
+    private void deleteActionEvent(ActionEvent e) {
+    	Driver driver = getDriver();
+    	int result = JOptionPane.showConfirmDialog(null,  "Delete " + driver.getName() + "?", "Delete Driver", JOptionPane.OK_CANCEL_OPTION);
+    	
+    	if (result == JOptionPane.OK_OPTION) {
+    		LinkedList<Integer> tourIDs = driver.getTourIDs();
+    		TourColl tours = CurrentProject.getTourColl();
+    		
+    		//remove driver from all scheduled tours
+			for (Tour t:driver.getTours()){
+				driver.delTour(t);
+			}
+    		
+    		drivers.del(driver.getID());
+    		
+    		try {
+				CurrentStorage.get().storeDriverList(CurrentProject.get(), drivers);
+				CurrentStorage.get().storeTourList(CurrentProject.get(), tours);
+				tableChanged();
+				
+				if (drivers.size() == 0) {
+					scheduleTable.setDriver(null);
+				}
+				
+				else {
+					scheduleTable.setDriver(getDriver());
+				}
+				
+				scheduleTable.tableChanged();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+    		
+			tableChanged();
+    	}
+    }
+    
+    /**
+     * Gets the currently selected Driver
+     * 
+     * @return the Driver
+     */
+    public Driver getDriver() {
+    	return (Driver) drivers.get((int) getValueAt(getSelectedRow(), ID_COLUMN));
+    }
+    
+    /**
+     * Sets the DriverScheduleTable to be updated when a user selects a driver in this table
+     * 
+     * @param scheduleTable The DriverScheduleTable
+     */
+    public void setScheduleTable(BusScheduleTable scheduleTable) {
+        this.scheduleTable = scheduleTable;
+    }
+}
